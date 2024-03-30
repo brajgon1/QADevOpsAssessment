5365f86d6434c5cf544d6dc3cbbdfaaabe0f5b2d
Brandon Gonzalez <brajgon@gmail.com>
2024-03-30 11:13:07 -0600

    second commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bots = require("./src/botsData");
 const shuffle = require("./src/shuffle");
 
@@ -9,6 +10,7 @@ const playerRecord = {
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 // Add up the total health of all the robots
 const calculateTotalHealth = (robots) =>
